52e10b81d9beee16aab436cf1a07ba7922b1363b
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-10 23:11:02 +0500

    some news

---
+++ src/components/SignUpForm/signup.style.ts
@@ -0,0 +1,19 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  paper: {
+    padding: theme.spacing(3),
+    width: 400,
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+  },
+}));

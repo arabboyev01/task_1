556193388e5275aaa2e70659e403fd3e3b2772e5
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-11 16:30:55 +0500

    changes

--- src/components/SignUpForm/signup.style.ts
+++ src/components/SignUpForm/signup.style.ts
@@ -1,19 +1,32 @@
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from '@mui/styles';
 
-export const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh',
-  },
-  paper: {
-    padding: theme.spacing(3),
-    width: 400,
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(2),
-  },
+export const useStyles = makeStyles(() => ({
+    mainBody: {
+        width: '100%',
+        height: '100vh',
+        background: '#333',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+    },
+    container: {
+        background: '#ccc',
+        padding: '100px 50px',
+    },
+    pop:{
+        margin: "10px 0",
+    },
+    paper: {
+        padding: '10px',
+        width: 300,
+    },
+    button: {
+        width: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
 }));

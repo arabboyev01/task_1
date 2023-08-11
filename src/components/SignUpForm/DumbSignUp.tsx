a1f879b9a3521d553e3a11edf173f55feb2500e0
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-11 16:28:28 +0500

    changes

--- src/components/SignUpForm/DumbSignUp.tsx
+++ src/components/SignUpForm/DumbSignUp.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
 import { ChildComponentProps } from '../../types'
-import { Box, InputLabel, Button } from '@mui/material'
+import { Box, InputLabel, Button, Typography } from '@mui/material'
 
-const DumSignUp: React.FC<ChildComponentProps> = ({handleSubmit}) => (
-    <Box>
-      <h2>Register User</h2>
-      <Form
-        onSubmit={handleSubmit}
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-              <Box>
-              <InputLabel>Firstname:</InputLabel>
-              <Field name="name" component="input" type="text" placeholder="Firstname"/>
-            </Box>
-            <Box>
-              <InputLabel>Email:</InputLabel>
-              <Field name="email" component="input" type="email" placeholder="Email"/>
-            </Box>
-            <Box>
-              <InputLabel>Password:</InputLabel>
-              <Field name="password" component="input" type="password" placeholder="Password"/>
-            </Box>
-            <Button type="submit">Register</Button>
-          </form>
-        )}
-      />
+const DumSignUp: React.FC<ChildComponentProps> = ({handleSubmit, styles}) => (
+    <Box className={styles.mainBody}>
+        <Box className={styles.container}>
+            <Typography variant="h5" gutterBottom> Register User </Typography>
+            <Form
+                onSubmit={handleSubmit}
+                render={({handleSubmit}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Box className={styles.pop}>
+                            <InputLabel >Firstname:</InputLabel>
+                            <Field name="name" component="input" type="text" placeholder="Firstname" className={styles.paper}/>
+                        </Box>
+                        <Box className={styles.pop}>
+                            <InputLabel>Email:</InputLabel>
+                            <Field name="email" component="input" type="email" placeholder="Email" className={styles.paper}/>
+                        </Box>
+                        <Box className={styles.pop}>
+                            <InputLabel>Password:</InputLabel>
+                            <Field name="secret" component="input" type="password" placeholder="Password" className={styles.paper}/>
+                        </Box>
+                        <Button type="submit" className={styles.button}>Register</Button>
+                    </form>
+                )}
+            />
+        </Box>
     </Box>
-  );
+);
 
 export default DumSignUp;

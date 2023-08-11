bcabc685b2bee678a4ba788d5c3c1f7d09e30dda
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-11 16:29:13 +0500

    changes

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,20 +1,22 @@
 import DumbSignUp from './DumbSignUp'
 import { api } from '../../config'
+import { useStyles } from "./signup.style"
 
 const SignUpForm = () => {
 
     const endpoint = 'signup'
+    const styles = useStyles();
     const handleSubmit = async (values: FormData) => {
         try {
             // @ts-ignore
-            const response = await api.makeSignupReq(endpoint, values.name, values.email, values.password);
+            const response = await api.makeSignupReq(endpoint, values.name, values.email, values.secret);
             console.log(response);
         } catch (err) {
             console.error(err);
         }
     };
 
-    return <DumbSignUp handleSubmit={handleSubmit}/>
+    return <DumbSignUp handleSubmit={handleSubmit} styles={styles}/>
 }
 
 export default SignUpForm
